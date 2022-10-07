fc73997d9840faf71a01512e344f76b623ef6bb9
sylvain <sguerrin549@gmail.com>
2022-10-07 20:45:02 +0200

    ✨ F01: LOGIN > Addtitle and form

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,5 +1,30 @@
+import { useState } from "react"
+
 export default function LoginPage() {
+//state
+const [inputValue, setInputValue] = useState("Bob")
+
+//comportement
+const handleSubmit = (event) => { 
+    event.preventDefault()
+    alert(`Bonjour ${inputValue}`)
+    setInputValue("")
+ }
+
+ const handleChange = (event) => { 
+    setInputValue(event.target.value)
+  }
+
+//affichage
   return (
-    <div>LoginPage</div>
+    <div>
+        <h1>Bienvenue chez nous</h1>
+        <br></br>
+        <h2>Connectez-vous</h2>
+        <form action="submit" onSubmit={handleSubmit}>
+            <input value={inputValue} onChange={handleChange} type="text" placeholder="Entrez votre prénom..." required />
+            <button>Accédez votre espace</button>
+        </form>
+    </div>
   )
 }
