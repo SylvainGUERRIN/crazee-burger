c4ec6ca4e44cad2e619e4d0bbcf24671b12a9fbb
sylvain <sguerrin549@gmail.com>
2022-12-03 20:12:40 +0100

    💄 F03: LoginForm > Add styles

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -20,25 +20,50 @@ export default function LoginForm() {
 
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous !</h1>
-      <br></br>
-      <h2>Connectez-vous</h2>
-      <input
-        value={inputValue}
-        onChange={handleChange}
-        type="text"
-        placeholder="Entrez votre prénom..."
-        required
-      />
-      <button>Accédez à votre espace</button>
+      <div>
+        <h1>Bienvenue chez nous !</h1>
+        <hr/>
+        <h2>Connectez-vous</h2>
+      </div>
+      <div>
+        <input
+          value={inputValue}
+          onChange={handleChange}
+          type="text"
+          placeholder="Entrez votre prénom..."
+          required
+        />
+        <button>Accédez à votre espace</button>
+      </div>
     </LoginFormStyled>
   );
   
 }
 
 const LoginFormStyled = styled.form`
-    border: 2px solid green;
-    background: red;
-    display: flex;
-    flex-direction: column;
+    //border: 2px solid green;
+    //background: red;
+    //display: flex;
+    //flex-direction: column;
+    text-align: center;
+    max-width: 500px;
+    min-width: 400px;
+    margin: 0 auto;
+    padding: 2.5rem 2rem;
+    border-radius: 5px;
+    font-family: "Amatic SC", cursive;
+
+    hr{
+      border: 1.5px solid #f56a2c;
+      margin-bottom: 40px;
+    }
+    h1{
+      color: #f56a2c;
+      font-size: 48px;
+    }
+    h2{
+      color: #8e8b8b;
+      margin: 20px 10px 10px;
+      font-size: 36px;
+    }
   `;
