b61fb00f61890726803e11862c3c470c17fba53e
sylvain <sguerrin549@gmail.com>
2022-12-04 15:25:14 +0100

    ♻️ F03: Login > LoginForm > create reusable PrimaryButton

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import {IoChevronForward} from 'react-icons/io5'
 import TextInput from "../../reusable-ui/TextInput";
 import {BsPersonCircle} from 'react-icons/bs'
+import PrimaryButton from "../../reusable-ui/PrimaryButton";
 
 export default function LoginForm() {
   const navigate = useNavigate()
@@ -36,10 +37,10 @@ export default function LoginForm() {
         required
         Icon={<BsPersonCircle className="icon"/>}
         />
-        <button className="button-with-icon">
-          <span>Accédez à votre espace</span>
-          <IoChevronForward className="icon"/>
-        </button>   
+        <PrimaryButton 
+        label={"Accédez à votre espace"} 
+        Icon={<IoChevronForward className="icon"/>}
+        /> 
       </div>
     </LoginFormStyled>
   );
@@ -72,47 +73,12 @@ const LoginFormStyled = styled.form`
       margin: 20px 10px 10px;
       font-size: 36px;
     }
-
-    .button-with-icon{
-      width: 100%;
-      border: 1px solid red;
-      display: inline-flex;
-      justify-content: center;
-      align-items: center;
-      position: relative;
-      white-space: nowrap;
-      text-decoration: none;
-      line-height: 1;
-
-      padding: 18px 24px;
-      border-radius: 5px;
-      font-size: 15px;
-      font-weight: 800;
-      color: white;
-      background-color: #ff9f1b;
-      border: 1px solid #ff9f1b;
-
-      &:hover:not(:disabled){
-        background-color: white;
-        color: #ff9f1b;
-        border: 1px solid #ff9f1b;
-        transition: all 200ms ease-out;
-      }
-      &:active{
-        color: white;
-        background-color: #ff9f1b;
-      }
-
-      &:disabled{
-        opacity: 0.6;
-        cursor: not-allowed;
-      }
-      .icon{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-size: 15px;
-        margin-left: 10px;
-      }
+    .icon{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 15px;
+    margin-left: 10px;
     }
+
   `;
