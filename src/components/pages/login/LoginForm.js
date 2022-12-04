ac22ccd902d50934728ceef7af11eb41cc874802
sylvain <sguerrin549@gmail.com>
2022-12-04 11:12:04 +0100

    💄 F03: Login > LoginForm > button add styles

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from 'styled-components';
 import {BsPersonCircle} from 'react-icons/bs'
+import {IoChevronForward} from 'react-icons/io5'
 
 export default function LoginForm() {
   const navigate = useNavigate()
@@ -37,7 +38,10 @@ export default function LoginForm() {
             required
           />
         </div>
-        <button>Accédez à votre espace</button>
+        <button className="button-with-icon">
+          <span>Accédez à votre espace</span>
+          <IoChevronForward className="icon"/>
+        </button>   
       </div>
     </LoginFormStyled>
   );
@@ -96,6 +100,48 @@ const LoginFormStyled = styled.form`
       &::placeholder{
         background: white;
         color: lightgrey;
+      }
+    }
+    .button-with-icon{
+      width: 100%;
+      border: 1px solid red;
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      white-space: nowrap;
+      text-decoration: none;
+      line-height: 1;
+
+      padding: 18px 24px;
+      border-radius: 5px;
+      font-size: 15px;
+      font-weight: 800;
+      color: white;
+      background-color: #ff9f1b;
+      border: 1px solid #ff9f1b;
+
+      &:hover:not(:disabled){
+        background-color: white;
+        color: #ff9f1b;
+        border: 1px solid #ff9f1b;
+        transition: all 200ms ease-out;
+      }
+      &:active{
+        color: white;
+        background-color: #ff9f1b;
+      }
+
+      &:disabled{
+        opacity: 0.6;
+        cursor: not-allowed;
+      }
+      .icon{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 15px;
+        margin-left: 10px;
       }
     }
   `;
