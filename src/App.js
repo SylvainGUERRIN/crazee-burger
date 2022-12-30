6f78c564fa92bedab74f835ab9ed70c3fc820896
sylvain <sguerrin549@gmail.com>
2022-12-30 19:14:40 +0100

    🎨 F07: Add AdminModeContext to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-//import { useState } from "react";
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import ErrorPage from "./components/pages/error/ErrorPage";
 import LoginPage from "./components/pages/login/LoginPage";
 import OrderPage from "./components/pages/order/OrderPage"
+import AdminModeContext from "./context/AdminModeContext"
 
 function App(){
+  const [adminMode, setAdminMode] = useState(false)
+  const adminModeContextValue = {
+    adminMode,
+    setAdminMode
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<LoginPage/>}/>
-      <Route path="/order/:name" element={<OrderPage/>}/>
-      <Route path="*" element={<ErrorPage/>}/>
-    </Routes>
+    <AdminModeContext.Provider value={adminModeContextValue}>
+      <Routes>
+        <Route path="/" element={<LoginPage/>}/>
+        <Route path="/order/:name" element={<OrderPage/>}/>
+        <Route path="*" element={<ErrorPage/>}/>
+      </Routes>
+    </AdminModeContext.Provider>
   )
 }
 
